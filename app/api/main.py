f10cd665368dc13dbdcb303282971e001af5eb8c
Tank1st-KT <BONES-9417@proton.me>
2025-12-25 22:34:28 +0700

    fix circular import: main app + routers

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from app.api.main import router as main_router
+
+from app.api.routes.main import router as main_router
 
 app = FastAPI(title="Mini-App TG API")
-
 app.include_router(main_router)
 
 @app.get("/health")
